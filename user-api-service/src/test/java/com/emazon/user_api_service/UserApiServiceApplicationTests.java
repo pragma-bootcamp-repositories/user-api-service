b927f6d757db1d2e3b596d1e79f8d33dc7c6d734
gamberfox <gamberfox@gmail.com>
2024-09-04 12:59:22 -0500

    setting up build gradle

---
+++ user-api-service/src/test/java/com/emazon/user_api_service/UserApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.user_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
