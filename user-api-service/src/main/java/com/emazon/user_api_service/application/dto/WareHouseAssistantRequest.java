0831e1ff6fc6aa6aa838e04fdeefd82cd1d0935e
gamberfox <gamberfox@gmail.com>
2024-09-20 18:02:31 -0500

    HU-007 progress

--- user-api-service/src/main/java/com/emazon/user_api_service/application/dto/WareHouseAssistantRequest.java
+++ user-api-service/src/main/java/com/emazon/user_api_service/application/dto/WareHouseAssistantRequest.java
@@ -1,6 +1,5 @@
 package com.emazon.user_api_service.application.dto;
 
-public class WareHouseAssistantRequest {
-    private String name;
-    private String lastName;
+public class WareHouseAssistantRequest extends UserDto{
+    Long phoneNumber;
 }
